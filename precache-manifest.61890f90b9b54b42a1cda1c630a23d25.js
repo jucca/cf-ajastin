a2a6bdb24e55f39bee083dd204844f3b1d2788c5
jucca <jukka.kin@gmail.com>
2020-04-18 18:02:54 +0300

    Updates

---
+++ precache-manifest.61890f90b9b54b42a1cda1c630a23d25.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5fb50e5f20cd6742e622fd66927365e4",
+    "url": "/cf-ajastin/index.html"
+  },
+  {
+    "revision": "463f7eb5ccf17280eddb",
+    "url": "/cf-ajastin/static/css/2.02c24866.chunk.css"
+  },
+  {
+    "revision": "4f91e530ddb569d98f42",
+    "url": "/cf-ajastin/static/css/main.1bcc77b1.chunk.css"
+  },
+  {
+    "revision": "463f7eb5ccf17280eddb",
+    "url": "/cf-ajastin/static/js/2.86f35d1e.chunk.js"
+  },
+  {
+    "revision": "8f16fffc7f685d9c2b50247b3f4402c7",
+    "url": "/cf-ajastin/static/js/2.86f35d1e.chunk.js.LICENSE"
+  },
+  {
+    "revision": "4f91e530ddb569d98f42",
+    "url": "/cf-ajastin/static/js/main.103863ec.chunk.js"
+  },
+  {
+    "revision": "9afa77dd856fdf3ad333",
+    "url": "/cf-ajastin/static/js/runtime-main.cf1a79db.js"
+  },
+  {
+    "revision": "a6bec1c4ef2d2fd1bac5a6951f5f32a3",
+    "url": "/cf-ajastin/static/media/TimerFont.a6bec1c4.ttf"
+  },
+  {
+    "revision": "e81081ce4df342950d7b46cd597ce28a",
+    "url": "/cf-ajastin/static/media/background-photo-mobile.e81081ce.jpg"
+  },
+  {
+    "revision": "91b867847b542f69f2995869f4e0d7f8",
+    "url": "/cf-ajastin/static/media/background-photo.91b86784.jpg"
+  },
+  {
+    "revision": "c5d5cada987a7ab193fb01c58593fa5c",
+    "url": "/cf-ajastin/static/media/beep.c5d5cada.mp3"
+  },
+  {
+    "revision": "eef56b309661a3cfb4d751674e4d6084",
+    "url": "/cf-ajastin/static/media/final_beep.eef56b30.mp3"
+  }
+]);
